d8c9948ff62862d5aa886d218a60f872a021674f
pgurung16 <40574990+pgurung16@users.noreply.github.com>
2018-06-25 12:51:18 -0400

    Create mixStart.java

---
+++ mixStart.java
@@ -0,0 +1,17 @@
+public boolean mixStart(String str) {
+  if (str == "mix") {
+  return true ;
+}
+    else if (str == "mix snacks") {
+      return true;
+    }
+    else if (str== "pix snacks") {
+      return true;
+    }
+    else if (str== "nix") {
+      return true;
+    }
+    else return false;
+}
+
+
